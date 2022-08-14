625af04023e262a28966f1147fd32ab54abd9da3
krolliaa <krolliaa@163.com>
2022-08-14 14:21:05 +0800

    cloud-demo: 4.order-service 通过 RestTemplate 远程调用 user-service 模块的 findById() 方法

--- 01-cloud-demo/order-service/src/main/java/com/kk/order/service/OrderService.java
+++ 01-cloud-demo/order-service/src/main/java/com/kk/order/service/OrderService.java
@@ -2,15 +2,25 @@ package com.kk.order.service;
 
 import com.kk.order.mapper.OrderMapper;
 import com.kk.order.pojo.Order;
+import com.kk.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long id) {
-        return orderMapper.findById(id);
+        Order order = orderMapper.findById(id);
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+        System.out.println(user);
+        return order;
     }
 }
